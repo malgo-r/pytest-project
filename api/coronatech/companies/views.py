c8ff3ab25e4f1805854c4e89bd52d272d6861471
malgo-r <malgodev@gmail.com>
2021-09-21 22:18:36 +0200

    Added send_company_email method

--- api/coronatech/companies/views.py
+++ api/coronatech/companies/views.py
@@ -1,5 +1,10 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+from django.core.mail import send_mail
 
 from api.coronatech.companies.models import Company
 from api.coronatech.companies.serializers import CompanySerializer
@@ -10,3 +15,14 @@ class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("-last_update")
     pagination_class = PageNumberPagination
+
+
+@api_view(http_method_names=['POST'])
+def send_company_email(request:Request) -> Response:
+    """
+    Send enami with request payload
+    sender: malgor.test@gmail.com
+    receiver: malgor.test@gmail.com
+    """
+    send_mail(subject=request.data.get("subject"), message=request.data.get("message"), from_email="malgor.test@gmail.com", recipient_list=["malgor.test@gmail.com"])
+    return Response({"status": "success", "info": "email sent successfully"}, status=200)
