36fb5e5f2f55b2fb98bfcd9268d0b0f2bfe69a5d
malgo-r <malgodev@gmail.com>
2021-09-23 21:02:46 +0200

    Added tests for cached fibonacci implementation

--- fibonacci/tests/test_fibonacci.py
+++ fibonacci/tests/test_fibonacci.py
@@ -1,9 +1,22 @@
 import pytest
+from typing import Callable
 
 from fibonacci.naive import fibonacci_naive
+from fibonacci.cached import fibonacci_cached
 
 
-@pytest.mark.parametrize("m, result", [(0, 0), (1, 1), (2, 1), (20, 6765), ],)
+@pytest.mark.parametrize("m, result", [(0, 0), (1, 1), (2, 1), (20, 6765), ], )
 def test_fibonacci_naive(m: int, result: int) -> None:
     res = fibonacci_naive(n=m)
+    assert res == result
+
+@pytest.mark.parametrize("m, result", [(0, 0), (1, 1), (2, 1), (20, 6765), ], )
+def test_fibonacci_cached(m: int, result: int) -> None:
+    res = fibonacci_cached(m)
+    return res == result
+
+@pytest.mark.parametrize("func", [fibonacci_naive, fibonacci_cached], )
+@pytest.mark.parametrize("m, result", [(0, 0), (1, 1), (2, 1), (20, 6765), ], )
+def test_fibonacci(func: Callable[[int], int], m: int, result: int) -> None:
+    res = fibonacci_naive(m)
     assert res == result
